a6fd948aef23a035d435337265c7f5d8cbabe62f
Diki Agung Mustaqim <agungmustaqimdiki@gmail.com>
2021-09-14 09:37:26 +0700

    finish

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,18 @@
 <template>
   <main v-if="!loading">
     <DataTitle :text="title" :dataDate="dataDate" />
+
+    <DataBoxes :stats="stats" />
+
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
+
+    <button
+      @click="clearCountryData"
+      v-if="stats.Country"
+      class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600"
+    >
+      Clear Country
+    </button>
   </main>
 
   <main class="flex flex-col align-center justify-center text-center" v-else>
@@ -14,11 +26,15 @@
 
 <script>
 import DataTitle from "@/components/DataTitle";
+import DataBoxes from "@/components/DataBoxes";
+import CountrySelect from "@/components/CountrySelect";
 
 export default {
   name: "Home",
   components: {
     DataTitle,
+    DataBoxes,
+    CountrySelect,
   },
   data() {
     return {
@@ -36,6 +52,19 @@ export default {
       const data = await res.json();
 
       return data;
+    },
+
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
+    },
+
+    async clearCountryData() {
+      this.loading = true;
+      const data = await this.fetchCovidData();
+      this.title = "Global";
+      this.stats = data.Global;
+      this.loading = false;
     },
   },
   async created() {
